f6117062e81c5422d99c842bb2488cbb6bdcef76
Roman Kunitsyn <roman.kunitsyn@gmail.com>
2018-08-23 14:13:26 +0300

    add readme and folder structure

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # patterns
 TypeScript implementation examples of common patterns
+
+
+## creational
+
+### abstract-factory 
+### builder 
+### factory-method 
+### prototype 
+### singleton 
+
+## structural
+
+### adapter
+### bridge 
+### composite 
+### decorator 
+### facade 
+### flyweight 
+### proxy 
+
+## behavioral
+
+### chain-of-responsibility 
+### command 
+### interpreter 
+### iterator 
+### mediator 
+### memento 
+### observer 
+### state 
+### strategy 
+### template-method 
+### visitor 
+
---
+++ generate-patterns-folder-structure.cmd
@@ -0,0 +1,32 @@
+mkdir creational
+cd creational
+mkdir abstract-factory 
+mkdir builder 
+mkdir factory-method 
+mkdir prototype 
+mkdir singleton 
+cd..
+mkdir structural
+cd structural
+mkdir adapter
+mkdir bridge 
+mkdir composite 
+mkdir decorator 
+mkdir facade 
+mkdir flyweight 
+mkdir proxy 
+cd..
+mkdir behavioral
+cd behavioral
+mkdir chain-of-responsibility 
+mkdir command 
+mkdir interpreter 
+mkdir iterator 
+mkdir mediator 
+mkdir memento 
+mkdir observer 
+mkdir state 
+mkdir strategy 
+mkdir template-method 
+mkdir visitor 
+cd..
